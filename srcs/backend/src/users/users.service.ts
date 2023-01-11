5de7487197c372975a2c1b9051345a133fa77781
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2023-01-11 18:10:53 +0100

    Added simple login structure

---
+++ srcs/backend/src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      id: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      id: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  findOneByUsername(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
