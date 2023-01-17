45040ba3f2d8186aa614c08edcb78966bca000aa
Ana Juncosa <ajuncosa@student.42madrid.com>
2023-01-17 12:51:18 +0100

    fix: seed data

--- srcs/backend/src/seed/seed.service.ts
+++ srcs/backend/src/seed/seed.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity'
-import { initialData } from './data/seed-data';
+import { initialData } from './seed-data/seed-data';
 
 @Injectable()
 export class SeedService {
