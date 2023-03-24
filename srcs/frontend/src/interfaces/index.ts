df7041b7d6072aa8c9ee4381fc90d6d1bd717eac
Ana Juncosa <ajuncosa@student.42madrid.com>
2023-03-24 14:04:49 +0100

    refactor: frontend interfaces folder

--- srcs/frontend/src/interfaces/index.ts
+++ srcs/frontend/src/interfaces/index.ts
@@ -1,6 +1,6 @@
-export type { Chat } from './chat.interface';
-export type { ChatUser } from './chat-user.interface';
-export type { Message } from './message.interface';
-export type { Channel } from './channel.interface';
+export type { Chat } from './chat/chat.interface';
+export type { ChatUser } from './chat/chat-user.interface';
+export type { Message } from './chat/message.interface';
+export type { Channel } from './chat/channel.interface';
 export type { JwtPayload } from './jwt-payload.interface';
 export type { UserData } from './user-data.interface';
