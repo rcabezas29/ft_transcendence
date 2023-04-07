051555fdc2b9c2ed8c4a7f7683d128540c400ae4
Adrian <adrian199909@gmail.com>
2023-04-07 11:02:21 +0200

    Add: App frame and router

--- srcs/frontend/src/App.vue
+++ srcs/frontend/src/App.vue
@@ -5,6 +5,8 @@
 	import router from './router';
 	import AlreadyConnected from './views/AlreadyConnected.vue';
 	import CursorLines from './components/CursorLines.vue';
+	import Button from "./components/ui/Button.vue"
+	import Chat from "./views/Chat.vue"
 
 	const routes = router.getRoutes();
 
@@ -26,6 +28,8 @@
 		<AlreadyConnected/>
 	</div>
 	<div v-else>
+
+		<!-- DEV START-->
 		<div class="dev">
 			<div class="dev-bar" @click="toggleDevPavel">DEV PANEL - CLICK TO OPEN</div>
 			<div :class="{ hidden: devPanelHidden}">
@@ -45,16 +49,48 @@
 				</div>
 				<button @click="logoutUser" v-if="user.checkIsLogged()">Logout</button>
 			</div>
-			
+		</div>
+		<!-- DEV END-->
+
+		<div class="app-container">
+			<div class="nav-header">
+				<div class="nav-buttons">
+					<router-link to="home">
+						<Button>PONG.EXE</Button>
+					</router-link>
+
+					<router-link to="social">
+						<Button>SOCIAL</Button>
+					</router-link>
+
+					<router-link to="profile">
+						<Button>PROFILE</Button>
+					</router-link>
+				</div>
+				<div class="user-info">
+					<div class="user-stats">
+						<div>username</div>
+						<div>elo: 69</div>
+					</div>
+					<div class="user-image">
+						<img :src=user.avatarImageURL />
+					</div>
+				</div>
+			</div>
+			<div class="app-body">
+				<router-view/>
+			</div>
+			<Chat/>
 		</div>
 
-		<router-view/>
 	</div>
 	
 </template>
 
 <style scoped>
 
+
+	/* DEV STYLE START */
 	.dev {
 		border: 1px solid black;
 	}
@@ -80,6 +116,45 @@
 		display: block;
 		width: 100px;
 		height: 100px;
+	}
+	/* DEV STYLE  END*/
+
+	.app-container {
+		box-sizing: border-box;
+		max-width: 1200px;
+		margin: auto;
+	}
+	
+	.nav-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: 30px;
+	}
+
+	.nav-buttons {
+		flex: 1;
+		display: flex;
+		gap: 30px;
+		margin-right: 15px;
+		max-width: 900px;
+	}
+
+	.user-info {
+		display: flex;
+		align-items: center;
+	}
+
+	.user-stats {
+		margin-right: 15px;
+	}
+	
+	.user-image {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 4px solid #4BFE65;
+		padding: 4px;
 	}
 
 </style>
--- srcs/frontend/src/views/Chat.vue
+++ srcs/frontend/src/views/Chat.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
 
-import { currentChat, chatIsChannel } from '../currentChat';
+	import { ref } from 'vue'; 
+	import { currentChat, chatIsChannel } from '../currentChat';
 
-import ChatFriendsSection from "../components/chat/ChatFriendsSection.vue";
-import ChatChannelsSection from "../components/chat/ChatChannelsSection.vue";
-import ChatMessages from "@/components/chat/ChatMessages.vue";
-import ChatChannelUserList from '@/components/chat/ChatChannelUserList.vue';
+	import ChatFriendsSection from "../components/chat/ChatFriendsSection.vue";
+	import ChatChannelsSection from "../components/chat/ChatChannelsSection.vue";
+	import ChatMessages from "@/components/chat/ChatMessages.vue";
+	import ChatChannelUserList from '@/components/chat/ChatChannelUserList.vue';
+
+	const chatOpened = ref(false);
+
+	function toggleChatWindow() {
+		chatOpened.value = !chatOpened.value;
+	}
 
 </script>
 
 <template>
-	<h1>Chat Component</h1>
-
 	<div class="chat-container">
-		<div>
-			<ChatFriendsSection />
-			<ChatChannelsSection />
+		<div class="chat-header" @click="toggleChatWindow">
+			CHAT.EXE
 		</div>
-		<div v-if="currentChat" class="chat-messages-container">
-			<ChatMessages />
-			<ChatChannelUserList v-if="chatIsChannel(currentChat)"/>
+		<div class="chat-body" v-if="chatOpened">
+			<div>
+				<ChatFriendsSection />
+				<ChatChannelsSection />
+			</div>
+			<div v-if="currentChat" class="chat-messages-container">
+				<ChatMessages />
+				<ChatChannelUserList v-if="chatIsChannel(currentChat)"/>
+			</div>
 		</div>
+		
 	</div>
 
 </template>
@@ -28,7 +39,23 @@ import ChatChannelUserList from '@/components/chat/ChatChannelUserList.vue';
 <style scoped>
 
 	.chat-container {
+		position: absolute;
+		bottom: 0;
+		max-width: 500px;
+		width: 100%;
+	}
+
+	.chat-header {
+		background-color: #1E9052;
+		border: 4px solid #4BFE65;
+		padding: 8px;
+		font-size: 18px;
+	}
+	
+	.chat-body {
 		display: flex;
+		border: 4px solid #4BFE65;
+		border-top: none;
 	}
 
 	.chat-messages-container {
