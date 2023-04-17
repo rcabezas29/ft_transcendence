0427fa5b5d8d31aa6f5489d7f7cec637db8dbda1
davyd11 <david.puente.r@gmail.com>
2023-04-17 23:01:04 +0200

    Fixes on box componet

--- srcs/frontend/src/components/profile/StatBox.vue
+++ srcs/frontend/src/components/profile/StatBox.vue
@@ -1,55 +1,63 @@
 <script setup lang="ts">
+  import { defineProps, withDefaults } from "vue";
 
-import { onBeforeMount, ref } from "vue";
-import { user } from "../../user"
-import Table from "../ui/Table.vue"
-import type { UserData } from "@/interfaces";
-import { computed } from "@vue/reactivity";
-import TextInputField from "../ui/TextInputField.vue";
+  interface Props {
+    title?: string;
+    stat?: string | number;
+  }
 
-interface Props {
-	title?: string,
-	stat?: string | number
-}
-
-const props = withDefaults(defineProps<Props>(), {
-	title: "N/A",
-	stat: "N/A",
-})
-
+  const props = withDefaults(defineProps<Props>(), {
+    title: "N/A",
+    stat: "N/A",
+  });
 </script>
 
 <template>
-	<div class="StatSquare">
-		<div class="BoxTitle">
-			{{ title }}<br>
-		</div>
-		<div class="BoxStat">
-			{{ stat }}
-		</div>
-	</div>
+  <div class="StatSquare">
+    <div class="BoxContent">
+      <div class="BoxTitle">
+        {{ props.title }}
+      </div>
+      <div class="BoxStat">
+        {{ props.stat }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .StatSquare {
-	display: flex;
-	width: justify-content;
-	align-items: center;
-	justify-content: center;
-	border: 4px solid #4BFE65;
-	padding: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 4px solid #4BFE65;
+  padding: 4px;
+  width: 100%;
+  height: 0;
+  padding-bottom: 100%;
+  position: relative;
+}
+
+.BoxContent {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 
 .BoxTitle {
-	font-size: 1.5em;
-	padding: 1em;
-	align-content: center;
+  font-size: 1.5vw;
+  padding: 0.5em 0;
 }
 
 .BoxStat {
-	font-size: 1em;
-	padding-top: 3em;
-	padding-bottom: 1em;
-	position: absolute;
+  font-size: 2.5vw;
+  padding: 0.5em 0;
 }
 </style>
