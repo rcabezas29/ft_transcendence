2cb1f41d9d79a05e1520adabb0756d4ef613f299
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2023-04-18 20:34:14 +0200

    Fixed game page distribution

--- srcs/frontend/src/views/SearchGame.vue
+++ srcs/frontend/src/views/SearchGame.vue
@@ -39,32 +39,39 @@ function cancelSearchGame() {
 
 <template>
 	<Modal :visible="searchGameModalVisible" @close="closeModal" title="MODAL TITLE">
-
 		<div class="radar-container">
 			<RadarAnimation/>
 		</div>
-
 		<Button style="width: 100%" @click="closeModal">CANCEL GAME SEARCH</Button>
-
 	</Modal>
-    <div class="search-game-box">
-        <h2>GAME MODE</h2>
-        <div class="mode-selector" v-if="gameController.state === GameState.None || gameController.state === GameState.Searching">
-          <Button @click="changeGameSelection(GameSelection.Original)" :selected="gameController.gameSelection === GameSelection.Original">ORIGINAL</Button>
-          <Button @click="changeGameSelection(GameSelection.SuperCool)" :selected="gameController.gameSelection === GameSelection.SuperCool">SUPER COOL</Button>
-          <Button @click="changeGameSelection(GameSelection.Crazy)" :selected="gameController.gameSelection === GameSelection.Crazy">CRAZY</Button>
-        </div>
-        <h2>PADDLE COLOR</h2>
-        <PaddleColorSelector/>
-    
-        <Button v-if="gameController.state !== GameState.Playing"  @click="() => {findGame(); openModal()}">SEARCH GAME</Button>
+
+  <div class="search-game-box">
+    <div class="search-game-customization">
+      <h2>GAME MODE</h2>
+      <div class="mode-selector" v-if="gameController.state === GameState.None || gameController.state === GameState.Searching">
+        <Button @click="changeGameSelection(GameSelection.Original)" :selected="gameController.gameSelection === GameSelection.Original">ORIGINAL</Button>
+        <Button @click="changeGameSelection(GameSelection.SuperCool)" :selected="gameController.gameSelection === GameSelection.SuperCool">SUPER COOL</Button>
+        <Button @click="changeGameSelection(GameSelection.Crazy)" :selected="gameController.gameSelection === GameSelection.Crazy">CRAZY</Button>
+      </div>
+      <h2>PADDLE COLOR</h2>
+      <PaddleColorSelector/>
     </div>
+      <Button v-if="gameController.state !== GameState.Playing"  @click="() => {findGame(); openModal()}">SEARCH GAME</Button>
+  </div>
+
 </template>
 
 <style scoped>
 
 .radar-container {
 	margin: 24px 0;
+}
+
+.search-game-box {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 @media only screen and (min-width: 850px) {
