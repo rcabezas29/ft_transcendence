b5a88eaa44847c0dddd31e8580ed09ef72d54346
David Puente <david.puenterodriguez@telefonica.com>
2023-04-23 12:04:19 +0200

    changes on the front of the friends component

--- srcs/frontend/src/components/friends/FriendsList.vue
+++ srcs/frontend/src/components/friends/FriendsList.vue
@@ -44,63 +44,71 @@ watch(friendsController, () => {
 
 <template>
     <div class="friends-section">
-        <div class="friends-subsection">
-            <h2>Active friends</h2>
-            <div class="active-friend" v-for="friend in activeFriends" :key="friend.userId">
-                <span>{{ friend.username }} ({{ friend.status }})</span>
-				<div class="friend-status" :class="{'friend-status-online': isOnline(friend), 'friend-status-gaming': isGaming(friend) }"></div>
-                <button @click="() => friendsController.blockUser(friend.userId)">Block</button>
-                <button @click="() => friendsController.unfriendUser(friend.userId)">Unfriend</button>
-            </div>
+        <div class="title-bar">
+            <h2>FRIENDS REQUESTS</h2>
         </div>
         <div class="friends-subsection">
-            <h2>Friends I've blocked</h2>
-            <div v-for="friend in blockedFriends" :key="friend.userId">
-                <span>{{ friend.username }}</span>
-                <button @click="() => friendsController.unblockUser(friend.userId)">Unblock</button>
-            </div>
-        </div>
-        <div class="friends-subsection">
-            <h2>Friend requests</h2>
-            <h3>Sent requests</h3>
-            <div v-for="friend in sentFriendRequests" :key="friend.userId">
-                <span>{{ friend.username }}</span>
-                <button @click="() => friendsController.unfriendUser(friend.userId)">Cancel</button>
-            </div>
-            <h3>Received requests</h3>
-            <div v-for="friend in receivedFriendRequests" :key="friend.userId">
+            <div class="friend-request" v-for="friend in receivedFriendRequests" :key="friend.userId">
                 <span>{{ friend.username }}</span>
                 <button @click="() => friendsController.acceptFriendRequest(friend.userId)">Accept</button>
                 <button @click="() => friendsController.denyFriendRequest(friend.userId)">Deny</button>
+            </div>
+        </div>
+        <div class="title-bar">
+            <h2># user status</h2>
+        </div>
+        <div class="friends-subsection">
+            <div class="active-friend" v-for="friend in activeFriends" :key="friend.userId">
+                <span>{{ friend.username }} ({{ friend.status }})</span>
+                <div class="friend-status"
+                    :class="{ 'friend-status-online': isOnline(friend), 'friend-status-gaming': isGaming(friend) }"></div>
+                <button @click="() => friendsController.blockUser(friend.userId)">Block</button>
+                <button @click="() => friendsController.unfriendUser(friend.userId)">Unfriend</button>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-    .friends-section {
-        width: 50%;
-        display: flex;
-        justify-content: space-around;
-        margin-bottom: 20px;
-    }
-    
-    .active-friend {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
+.friends-section {
+    width: 100%;
+    padding: 1rem;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
 
-    .friend-status {
-        height:10px;
-        width: 10px;
-    }
+.title-bar {
+    /* width: 100%;
+        background-color: #1E9052;
+        border-color: #4BFE65;
+        color: white;
+        padding: 0.5rem;
+        text-align: center;
+        font-size: 1.25rem; */
+    box-sizing: border-box;
+    font-size: 0.6rem;
+    text-align: center;
+    padding: 0.1rem;
+    background: #1E9052;
+    border: 0.3em solid #4BFE65;
+}
 
-    .friend-status-online {
-        background-color: #07d807;
-    }
+.friends-subsection {
+    width: 100%;
+}
 
-    .friend-status-gaming {
-        background-color: #8807d8;
-    }
+.active-friend,
+.friend-request {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0.5rem;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    margin-bottom: 0
+}
 </style>
