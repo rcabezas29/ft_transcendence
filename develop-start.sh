d17dedd5e42802e116c53d61665edf0ae2f7e084
Ana Juncosa <ajuncosa@student.42madrid.com>
2023-05-06 11:14:10 +0200

    fix: develop scripts for docker compose version; delete: .env-dev

--- develop-start.sh
+++ develop-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
--- develop-stop.sh
+++ develop-stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml -f docker-compose.dev.yml down -v
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml down -v
